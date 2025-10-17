7181caa08b11b968bde5c4efcdc191f14e305214
YenMur <yennifer.murillo@uao.edu.co>
2025-10-17 15:49:03 -0500

    Cambios

---
+++ Assets/Recursos/Audios/Volumen/Mute.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Mute : MonoBehaviour
+{
+    [Header("Botón de mute")]
+    [SerializeField] private Button muteButton;
+    [SerializeField] private Sprite iconMute;
+    [SerializeField] private Sprite iconUnmute;
+
+    private bool isMuted = false;
+
+    void Start()
+    {
+        if (muteButton != null)
+            muteButton.onClick.AddListener(ToggleMute);
+
+        UpdateIcon();
+    }
+
+    private void ToggleMute()
+    {
+        isMuted = !isMuted;
+
+        // 🔇 Control global del audio
+        AudioListener.volume = isMuted ? 0f : 1f;
+
+        UpdateIcon();
+    }
+
+    private void UpdateIcon()
+    {
+        if (muteButton != null && muteButton.image != null)
+        {
+            muteButton.image.sprite = isMuted ? iconMute : iconUnmute;
+        }
+    }
+}
